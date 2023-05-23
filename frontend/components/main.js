d8ac18f2ba47f1cf33328415fcddbac05eb66fe8
Jossif Elefteriadis <jossif_@hotmail.com>
2023-05-23 19:23:54 +0200

    change color and margin on title

--- frontend/components/main.js
+++ frontend/components/main.js
@@ -4,7 +4,9 @@ export default function Main() {
   return (
     <section className="w-full h-screen text-center bg-bgImage bg-cover bg-no-repeat">
       <section className="max-w-[1240px] w-full h-full mx-auto p-2 flex flex-col justify-center items-center">
-        <h1>Welcome to the BTR Investment Club</h1>
+        <h1 className="text-white drop-shadow-lg mb-24">
+          Welcome to the BTR Investment Club
+        </h1>
       </section>
     </section>
   );
