8653f0e2ce65a43543eebecb77e484fef4182db7
Jossif Elefteriadis <jossif_@hotmail.com>
2023-05-24 14:28:52 +0200

    add to faq

--- frontend/components/learn.js
+++ frontend/components/learn.js
@@ -140,8 +140,182 @@ export default function Learn() {
                     meetings with Brando and Sparks Flames.
                   </p>
                 </section>
+                <section className="mb-10">
+                  <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
+                    <svg
+                      className="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-gray-400"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
+                        clip-rule="evenodd"
+                      ></path>
+                    </svg>
+                    How often will voting on investment proposals occur?
+                  </h3>
+                  <p className="text-gray-500 dark:text-gray-400">
+                    Voting on investment proposals will occur on a regular
+                    basis, depending on the availability of new opportunities
+                    and the needs of the club. The frequency of voting will be
+                    communicated to the members through official channels.
+                  </p>
+                </section>
+                <section className="mb-10">
+                  <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
+                    <svg
+                      className="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-gray-400"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
+                        clip-rule="evenodd"
+                      ></path>
+                    </svg>
+                    Can members propose investments outside of the predetermined
+                    projects?
+                  </h3>
+                  <p className="text-gray-500 dark:text-gray-400">
+                    Yes, members can propose investments outside of the
+                    predetermined projects. All investment proposals will be
+                    reviewed and voted upon by the BTR investors to determine
+                    their viability and alignment with the club&apos;s goals.
+                  </p>
+                </section>
+                <section className="mb-10">
+                  <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
+                    <svg
+                      className="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-gray-400"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
+                        clip-rule="evenodd"
+                      ></path>
+                    </svg>
+                    How will the Investment Club handle potential risks and due
+                    diligence?
+                  </h3>
+                  <p className="text-gray-500 dark:text-gray-400">
+                    The BTR Investment Club takes risk management and due
+                    diligence seriously. Before any investment decision is made,
+                    thorough research and analysis will be conducted to evaluate
+                    the potential risks and rewards. The club will aim to make
+                    informed investment choices to protect the interests of its
+                    members.
+                  </p>
+                </section>
               </section>
               <section>
+                <section className="mb-10">
+                  <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
+                    <svg
+                      className="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-gray-400"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
+                        clip-rule="evenodd"
+                      ></path>
+                    </svg>
+                    Are there any fees or costs associated with joining the BTR
+                    Investment Club?
+                  </h3>
+                  <p className="text-gray-500 dark:text-gray-400">
+                    There are no fees or costs associated with joining the BTR
+                    Investment Club itself. However, members are responsible for
+                    their own expenses related to participating in investment
+                    opportunities, such as transaction fees and costs associated
+                    with buying tokens or assets.
+                  </p>
+                </section>
+                <section className="mb-10">
+                  <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
+                    <svg
+                      className="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-gray-400"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
+                        clip-rule="evenodd"
+                      ></path>
+                    </svg>
+                    Will the BTR Investment Club provide investment advice or
+                    financial recommendations?
+                  </h3>
+                  <p className="text-gray-500 dark:text-gray-400">
+                    The BTR Investment Club is a platform for collective
+                    decision-making, and it does not provide personalized
+                    investment advice or financial recommendations. Members are
+                    encouraged to do their own research and seek professional
+                    advice before making any investment decisions.
+                  </p>
+                </section>
+                <section className="mb-10">
+                  <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
+                    <svg
+                      className="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-gray-400"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
+                        clip-rule="evenodd"
+                      ></path>
+                    </svg>
+                    Can members withdraw their investment from the club?
+                  </h3>
+                  <p className="text-gray-500 dark:text-gray-400">
+                    Once an investment has been made by the club, it is
+                    generally expected to be held for the agreed-upon duration.
+                    However, exceptional circumstances may be considered on a
+                    case-by-case basis. Members can reach out to the club
+                    administrators for further information and guidance
+                    regarding investment withdrawal.
+                  </p>
+                </section>
+                <section className="mb-10">
+                  <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
+                    <svg
+                      className="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-gray-400"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
+                        clip-rule="evenodd"
+                      ></path>
+                    </svg>
+                    How can I stay updated on the activities of the BTR
+                    Investment Club?
+                  </h3>
+                  <p className="text-gray-500 dark:text-gray-400">
+                    Regular updates, announcements, and discussions related to
+                    the BTR Investment Club will be shared through official
+                    channels, including the Discord group and other designated
+                    communication platforms. Members are encouraged to actively
+                    engage in the community to stay informed and participate in
+                    the decision-making process.
+                  </p>
+                </section>
                 <section className="mb-10">
                   <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
                     <svg
