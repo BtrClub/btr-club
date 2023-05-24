61fef4966cef3de6a7d58cd92650b5833b188677
Jossif Elefteriadis <jossif_@hotmail.com>
2023-05-24 20:26:37 +0200

    refactor about

--- frontend/components/about.js
+++ frontend/components/about.js
@@ -4,28 +4,36 @@ import VideoTestimonial from "./videoTestimonial";
 export default function About() {
   return (
     <section className="w-full p-2 flex flex-col items-center pt-28 py-16">
-      <h1>About</h1>
+      <h1>About the BTR Investment Club</h1>
       <section className="max-w-[1240px] m-auto md:grid grid-cols-2 gap-8 items-center mt-14 mb-20">
         <section>
           <p>
-            The BTR Investment Club is created to help the BTR community members
-            share in different investment opportunities they couldn&apos;t
-            otherwise take part in. It&apos;s a collective effort to find
-            opportunities and pool our money together for future benefits based
-            on a NFT voting system. This can create residual income for all
-            members who partake in this Investment Club.
+            The BTR Investment Club is a dedicated platform created to empower
+            and unite the members of the BTR community in exploring investment
+            opportunities that may otherwise be inaccessible individually. It is
+            a collective effort aimed at pooling our resources and leveraging
+            the power of NFT voting to make informed investment decisions. By
+            joining forces, we can potentially generate residual income and
+            unlock future benefits for all club members.
           </p>
           <br />
           <p>
-            The BTR community was started in 2019. I (Brando) saw a void of
-            trustworthy crypto influencers who gave real value. Feeling like God
-            led me to this opportunity, I vowed to be used as a vessel and a
-            guide to those who would need proper navigation of the crypto wild
-            west. A viewer left a comment/compliment on a video saying they
-            would pay a monthly subscription for two deep dive videos a month. A
-            Patreon group was formed, and members who resonated with the
-            material and mission started to pile in. The same energy and mission
-            have continued even now in 2023.
+            The BTR community was founded in 2019 with a vision to provide
+            trustworthy and value-driven insights in the ever-evolving world of
+            cryptocurrencies. Brando, the founder, recognized the need for
+            reliable crypto influencers and committed himself to be a guiding
+            light for those seeking proper navigation in the crypto wild west.
+            It was a calling that he embraced, driven by a sense of purpose to
+            serve as a vessel for knowledge and guidance.
+          </p>
+          <br />
+          <p>
+            The community&apos;s journey began when a viewer expressed their
+            willingness to pay for exclusive deep-dive videos. This led to the
+            formation of a Patreon group, where like-minded individuals joined
+            forces, resonating with the shared mission and material. Since then,
+            the BTR community has grown in strength and continues to uphold the
+            same energy, dedication, and mission in 2023.
           </p>
         </section>
         <section>
@@ -36,13 +44,15 @@ export default function About() {
           </section>
           <br />
           <p>
-            The Bigger Than Race “BTR” community comprises people from all walks
-            of life. We share a common goal of achieving success, breaking
-            generational curses and shifting our perspectives to become better
-            investors and people through blockchain technology. Spirituality is
-            at the forefront of everything we do, which helps guide the
-            community members in the best infrastructure assets to own for the
-            Fourth Industrial Revolution.
+            Join us in the BTR Investment Club and be part of a community that
+            strives for financial growth, education, and collective empowerment.
+            Together, we can navigate the ever-changing crypto landscape and
+            make a lasting impact on our financial futures.
+          </p>
+          <br />
+          <p>
+            Let&apos;s embrace this ongoing journey and seize the opportunities
+            that lie ahead!
           </p>
         </section>
       </section>
