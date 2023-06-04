d5ef98f3aa08dfca968aec7ab2c962a1380567fb
Jossif Elefteriadis <jossif_@hotmail.com>
2023-06-04 21:34:33 +0200

    Fix mobile nav

--- frontend/components/nav.js
+++ frontend/components/nav.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
@@ -6,27 +6,88 @@ import { ConnectButton } from "@rainbow-me/rainbowkit";
 import Logo from "../public/assets/btrLogo.png";
 
 export default function Nav() {
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
   return (
     <section className="bg-gray-800/90 fixed w-full h-20 shadow-xl z-[100]">
       <section className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
         <Link href="/">
           <Image src={Logo} alt="Logo image" width="60" height="60" />
         </Link>
-        <section>
-          <ul className="hidden md:flex items-center mr-4 text-[#ecf0f3]">
-            <li className="ml-10 text-sm uppercase hover:border-b">
+        <section className="sm:hidden flex items-center">
+          <section className="mr-4 text-xs sm:text-sm uppercase">
+            <ConnectButton />
+          </section>
+          <section
+            className="HAMBURGER-ICON space-y-2"
+            onClick={() => setIsNavOpen((prev) => !prev)}
+          >
+            <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
+            <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
+            <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
+          </section>
+
+          <section
+            className={
+              isNavOpen
+                ? "absolute w-5/6 h-screen top-0 right-0 bg-black flex flex-col justify-evenly items-center"
+                : "hidden"
+            }
+          >
+            <section
+              className="absolute top-0 right-0 px-8 py-8"
+              onClick={() => setIsNavOpen(false)}
+            >
+              <svg
+                className="h-8 w-8 text-gray-600"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <line x1="18" y1="6" x2="6" y2="18" />
+                <line x1="6" y1="6" x2="18" y2="18" />
+              </svg>
+            </section>
+            <ul className="flex flex-col items-center justify-between min-h-[250px]">
+              <li className="border-b border-gray-400 my-8 uppercase">
+                <Link href="/learn">Learn</Link>
+              </li>
+              <li className="border-b border-gray-400 my-8 uppercase">
+                <Link href="/about">About</Link>
+              </li>
+              <li className="border-b border-gray-400 my-8 uppercase">
+                <Link href="/community">Community</Link>
+              </li>
+              <li className="border-b border-gray-400 my-8 uppercase">
+                <Link href="/vote">Vote</Link>
+              </li>
+              <li className="border-b border-gray-400 my-8 uppercase">
+                <Link href="/community">Community</Link>
+              </li>
+            </ul>
+            {/* <section className="ml-4 sm:ml-10 text-xs sm:text-sm uppercase">
+              <ConnectButton />
+            </section> */}
+          </section>
+        </section>
+        <section className="hidden sm:flex">
+          <ul className="flex items-center mr-4 text-[#ecf0f3]">
+            <li className="ml-4 sm:ml-10 text-xs sm:text-sm uppercase hover:border-b">
               <Link href="/learn">Learn</Link>
             </li>
-            <li className="ml-10 text-sm uppercase hover:border-b flex items-center">
+            <li className="ml-4 sm:ml-10 text-xs sm:text-sm uppercase hover:border-b flex items-center">
               <Link href="/about">About</Link>
             </li>
-            <li className="ml-10 text-sm uppercase hover:border-b">
+            <li className="ml-4 sm:ml-10 text-xs sm:text-sm uppercase hover:border-b">
               <Link href="/community">Community</Link>
             </li>
-            <li className="ml-10 text-sm uppercase hover:border-b">
+            <li className="ml-4 sm:ml-10 text-xs sm:text-sm uppercase hover:border-b">
               <Link href="/vote">Vote</Link>
             </li>
-            <li className="ml-10 text-sm uppercase">
+            <li className="ml-4 sm:ml-10 text-xs sm:text-sm uppercase">
               <ConnectButton />
             </li>
           </ul>
--- frontend/components/vote.js
+++ frontend/components/vote.js
@@ -139,7 +139,7 @@ export default function Vote() {
     <section className="pt-28">
       {!isLoggedIn ? (
         <section className="min-h-screen flex flex-col items-center mt-8">
-          <section className="w-2/6 flex justify-between items-center">
+          <section className="w-full md:w-2/6 flex justify-between items-center">
             <AlertTriangle fontSize="80px" className="text-red-600" />
             <section className="w-84 h-56 flex flex-col justify-center text-2xl text-center">
               <p className="mb-1">
@@ -171,116 +171,119 @@ export default function Vote() {
           </section>
         </section>
       ) : (
-        <section className="flex justify-center pb-2 px-10 bg-black border-b border-gray-500">
-          <section className="w-2/3 sm:w-2/4 lg:w-1/3 flex content-center justify-evenly">
-            <p className="cursor-pointer hover:text-pink-600 text-lg">
-              <Link href="/vote" className="text-pink-600">
-                Proposals
-              </Link>
-            </p>
-            <p className="cursor-pointer hover:text-pink-600 text-lg">
-              <Link href="/createproposal">Create Proposal</Link>
-            </p>
+        <section>
+          <section className="flex justify-center pb-2 px-10 bg-black border-b border-gray-500">
+            <section className="w-2/3 sm:w-2/4 lg:w-1/3 flex content-center justify-evenly">
+              <p className="cursor-pointer hover:text-pink-600 text-lg">
+                <Link href="/vote" className="text-pink-600">
+                  Proposals
+                </Link>
+              </p>
+              <p className="cursor-pointer hover:text-pink-600 text-lg">
+                <Link href="/createproposal">Create Proposal</Link>
+              </p>
+            </section>
+          </section>
+          <section className="flex flex-col items-center bg-white pt-8 pb-20">
+            <section className="w-2/3">
+              <section className="flex justify-evenly w-full">
+                <section className="w-80 h-28 bg-waves bg-blue-900 flex flex-col justify-evenly pl-4 pb-4 border rounded-md border-gray-200 text-gray-900 shadow-lg">
+                  <p className="text-gray-100 text-lg">Total Proposals</p>
+                  <p className="text-white text-lg font-semibold">
+                    {proposalCount} total proposals
+                  </p>
+                </section>
+                <section className="w-80 h-28 bg-waves bg-blue-900 flex flex-col justify-evenly pl-4 pb-4 border rounded-md border-gray-200 text-gray-900 shadow-lg">
+                  <p className="text-gray-100 text-lg">Participation</p>
+                  <p className="text-white text-lg font-semibold">
+                    {voteCount} total votes
+                  </p>
+                </section>
+                <section className="w-80 h-28 bg-waves bg-blue-900 flex flex-col justify-evenly pl-4 pb-4 border rounded-md border-gray-200 text-gray-900 shadow-lg">
+                  <p className="text-gray-100 text-lg">Active Proposals</p>
+                  <p className="text-white text-lg font-semibold">
+                    {activeProposals} active proposals
+                  </p>
+                </section>
+              </section>
+              <section className="w-full min-h-screen flex flex-col items-center mt-6 py-2">
+                {proposals.map((proposal, i) => {
+                  return (
+                    <section
+                      className="flex flex-col justify-between w-4/5 h-24 my-4 px-4 py-2 rounded-md shadow-md shadow-gray-200 hover:shadow-lg hover:shadow-gray-300 hover:border-gray-100 hover:border cursor-pointer"
+                      key={i}
+                      onClick={() =>
+                        setProposalContext(
+                          proposal.title,
+                          proposal.description,
+                          proposal.owner,
+                          proposal.deadline,
+                          proposal.active,
+                          proposal.totalVotes,
+                          proposal.yesVotes,
+                          proposal.noVotes
+                        )
+                      }
+                    >
+                      <Link
+                        href={`/proposal/${proposal.title}`}
+                        className="h-full flex flex-col justify-between"
+                      >
+                        <span className="text-slate-900 text-3xl font-semibold">
+                          {proposal.title}
+                        </span>
+                        <section className="flex justify-between text-sm">
+                          <section className="flex items-center">
+                            {!proposal.active ? (
+                              <section className="flex items-center bg-green-100 px-2 mr-2 rounded-2xl">
+                                <span>
+                                  <Checkmark
+                                    fontSize="13px"
+                                    className="fill-green-500 mr-1"
+                                  />
+                                </span>
+                                <span className="text-green-500 mr-2 font-semibold">
+                                  ACTIVE
+                                </span>
+                              </section>
+                            ) : (
+                              <section className="flex items-center bg-red-100 px-2 mr-2 rounded-2xl">
+                                <span>
+                                  <CrossCircle
+                                    fontSize="17px"
+                                    className="fill-red-500 mr-1"
+                                  />
+                                </span>
+                                <span className="text-red-500 mr-2 font-semibold">
+                                  FINISHED
+                                </span>
+                              </section>
+                            )}
+                            <span className="text-slate-400 mr-2">
+                              {proposal.totalVotes} VOTES
+                            </span>
+                            <span className="text-slate-700">
+                              ENDS ON {proposal.deadline}
+                            </span>
+                          </section>
+                          <span className="text-slate-400">
+                            LEADING:{" "}
+                            <span className="text-slate-700 font-semibold">
+                              {proposal.yesVotes > proposal.noVotes
+                                ? "YES"
+                                : "NO"}
+                            </span>
+                          </span>
+                        </section>
+                      </Link>
+                    </section>
+                  );
+                })}
+              </section>
+            </section>
           </section>
         </section>
       )}
-
-      <section className="flex flex-col items-center bg-white pt-8 pb-20">
-        <section className="w-2/3">
-          <section className="flex justify-evenly w-full">
-            <section className="w-80 h-28 bg-waves bg-blue-900 flex flex-col justify-evenly pl-4 pb-4 border rounded-md border-gray-200 text-gray-900 shadow-lg">
-              <p className="text-gray-100 text-lg">Total Proposals</p>
-              <p className="text-white text-lg font-semibold">
-                {proposalCount} total proposals
-              </p>
-            </section>
-            <section className="w-80 h-28 bg-waves bg-blue-900 flex flex-col justify-evenly pl-4 pb-4 border rounded-md border-gray-200 text-gray-900 shadow-lg">
-              <p className="text-gray-100 text-lg">Participation</p>
-              <p className="text-white text-lg font-semibold">
-                {voteCount} total votes
-              </p>
-            </section>
-            <section className="w-80 h-28 bg-waves bg-blue-900 flex flex-col justify-evenly pl-4 pb-4 border rounded-md border-gray-200 text-gray-900 shadow-lg">
-              <p className="text-gray-100 text-lg">Active Proposals</p>
-              <p className="text-white text-lg font-semibold">
-                {activeProposals} active proposals
-              </p>
-            </section>
-          </section>
-          <section className="w-full min-h-screen flex flex-col items-center mt-6 py-2">
-            {proposals.map((proposal, i) => {
-              return (
-                <section
-                  className="flex flex-col justify-between w-4/5 h-24 my-4 px-4 py-2 rounded-md shadow-md shadow-gray-200 hover:shadow-lg hover:shadow-gray-300 hover:border-gray-100 hover:border cursor-pointer"
-                  key={i}
-                  onClick={() =>
-                    setProposalContext(
-                      proposal.title,
-                      proposal.description,
-                      proposal.owner,
-                      proposal.deadline,
-                      proposal.active,
-                      proposal.totalVotes,
-                      proposal.yesVotes,
-                      proposal.noVotes
-                    )
-                  }
-                >
-                  <Link
-                    href={`/proposal/${proposal.title}`}
-                    className="h-full flex flex-col justify-between"
-                  >
-                    <span className="text-slate-900 text-3xl font-semibold">
-                      {proposal.title}
-                    </span>
-                    <section className="flex justify-between text-sm">
-                      <section className="flex items-center">
-                        {!proposal.active ? (
-                          <section className="flex items-center bg-green-100 px-2 mr-2 rounded-2xl">
-                            <span>
-                              <Checkmark
-                                fontSize="13px"
-                                className="fill-green-500 mr-1"
-                              />
-                            </span>
-                            <span className="text-green-500 mr-2 font-semibold">
-                              ACTIVE
-                            </span>
-                          </section>
-                        ) : (
-                          <section className="flex items-center bg-red-100 px-2 mr-2 rounded-2xl">
-                            <span>
-                              <CrossCircle
-                                fontSize="17px"
-                                className="fill-red-500 mr-1"
-                              />
-                            </span>
-                            <span className="text-red-500 mr-2 font-semibold">
-                              FINISHED
-                            </span>
-                          </section>
-                        )}
-                        <span className="text-slate-400 mr-2">
-                          {proposal.totalVotes} VOTES
-                        </span>
-                        <span className="text-slate-700">
-                          ENDS ON {proposal.deadline}
-                        </span>
-                      </section>
-                      <span className="text-slate-400">
-                        LEADING:{" "}
-                        <span className="text-slate-700 font-semibold">
-                          {proposal.yesVotes > proposal.noVotes ? "YES" : "NO"}
-                        </span>
-                      </span>
-                    </section>
-                  </Link>
-                </section>
-              );
-            })}
-          </section>
-        </section>
-      </section>
     </section>
   );
 }
