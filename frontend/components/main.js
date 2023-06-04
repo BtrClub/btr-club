47c04176dbfec0e5c7803f860cdf1c22910ab0b9
Jossif Elefteriadis <jossif_@hotmail.com>
2023-06-04 20:52:26 +0200

    Refactor

--- frontend/components/main.js
+++ frontend/components/main.js
@@ -5,7 +5,7 @@ export default function Main() {
     <section className="w-full h-screen text-center bg-bgImage bg-cover bg-no-repeat">
       <section className="w-full h-full flex flex-col justify-center items-center">
         <h1 className="text-white drop-shadow-lg mb-24">
-          Welcome to the BTR Investment Club
+          Welcome To The BTR Investment Club
         </h1>
       </section>
     </section>
