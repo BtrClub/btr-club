73e337683d1345741ea6f764b839a2f49d234e88
Jossif Elefteriadis <jossif_@hotmail.com>
2023-06-22 20:40:12 +0200

    change claim bar text color

--- frontend/components/claimBar.js
+++ frontend/components/claimBar.js
@@ -6,7 +6,7 @@ export default function claimBar() {
     <section className="w-full mt-10 text-center relative">
       <section className="w-full h-16 dark:bg-gray-900 fixed bottom-0 flex justify-center items-center">
         <Link href="/claim">
-          <h1 className="text-xl text-white hover:text-gray-400 drop-shadow-lg cursor-pointer">
+          <h1 className="text-xl text-lime-400 hover:text-lime-500 drop-shadow-lg cursor-pointer">
             CLAIM YOUR BTR INVESTMENT CLUB NFT
           </h1>
         </Link>
